12f2e3a2b82444bcfd7efbb491f5e2446357dc7b
Ariadne Engelbrecht <ariadneengelbrecht@dionysos.local>
2023-08-16 10:28:10 +0200

    Configure basic dev mail service.

--- config/runtime.exs
+++ config/runtime.exs
@@ -105,6 +105,13 @@ if config_env() == :prod do
   #       adapter: Swoosh.Adapters.Mailgun,
   #       api_key: System.get_env("MAILGUN_API_KEY"),
   #       domain: System.get_env("MAILGUN_DOMAIN")
+
+  # By default it uses the "Local" adapter which stores the emails
+  # locally. You can see the emails in your browser, at "/dev/mailbox".
+  config :cf_portal, ThreatShield.Mailer, adapter: Swoosh.Adapters.Local
+
+  # Swoosh API client is needed for adapters other than SMTP.
+  config :swoosh, :api_client, false
   #
   # For this example you need include a HTTP client required by Swoosh API client.
   # Swoosh supports Hackney and Finch out of the box:
