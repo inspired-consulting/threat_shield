bac5406505277db412c82516e22bfd55abdb2c87
Ariadne Engelbrecht <ariadneengelbrecht@dionysos.local>
2023-09-07 07:14:27 +0200

    Fix heroicons.

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -20,7 +20,7 @@ module.exports = {
           300: "#8086FF",
           400: "#4D59FF",
           500: "#1A1C8E",
-          600: "#17196B", // Default primary color
+          600: "#17196B",
           700: "#13134D",
           800: "#0E0F36",
           900: "#0A0B24",
@@ -30,7 +30,7 @@ module.exports = {
           200: "#FFF3B3",
           300: "#FFEB80",
           400: "#FFE34D",
-          500: "#D4AF34", // Default secondary color
+          500: "#D4AF34",
           600: "#A08026",
           700: "#71501A",
           800: "#482E0E",
@@ -38,52 +38,52 @@ module.exports = {
         },
       },
     },
-    plugins: [
-      require("@tailwindcss/forms"),
-      // Allows prefixing tailwind classes with LiveView classes to add rules
-      // only when LiveView classes are applied, for example:
-      //
-      //     <div class="phx-click-loading:animate-ping">
-      //
-      plugin(({ addVariant }) => addVariant("phx-no-feedback", [".phx-no-feedback&", ".phx-no-feedback &"])),
-      plugin(({ addVariant }) => addVariant("phx-click-loading", [".phx-click-loading&", ".phx-click-loading &"])),
-      plugin(({ addVariant }) => addVariant("phx-submit-loading", [".phx-submit-loading&", ".phx-submit-loading &"])),
-      plugin(({ addVariant }) => addVariant("phx-change-loading", [".phx-change-loading&", ".phx-change-loading &"])),
+  },
+  plugins: [
+    require("@tailwindcss/forms"),
+    // Allows prefixing tailwind classes with LiveView classes to add rules
+    // only when LiveView classes are applied, for example:
+    //
+    //     <div class="phx-click-loading:animate-ping">
+    //
+    plugin(({ addVariant }) => addVariant("phx-no-feedback", [".phx-no-feedback&", ".phx-no-feedback &"])),
+    plugin(({ addVariant }) => addVariant("phx-click-loading", [".phx-click-loading&", ".phx-click-loading &"])),
+    plugin(({ addVariant }) => addVariant("phx-submit-loading", [".phx-submit-loading&", ".phx-submit-loading &"])),
+    plugin(({ addVariant }) => addVariant("phx-change-loading", [".phx-change-loading&", ".phx-change-loading &"])),
 
-      // Embeds Heroicons (https://heroicons.com) into your app.css bundle
-      // See your `CoreComponents.icon/1` for more information.
-      //
-      plugin(function ({ matchComponents, theme }) {
-        let iconsDir = path.join(__dirname, "./vendor/heroicons/optimized")
-        let values = {}
-        let icons = [
-          ["", "/24/outline"],
-          ["-solid", "/24/solid"],
-          ["-mini", "/20/solid"]
-        ]
-        icons.forEach(([suffix, dir]) => {
-          fs.readdirSync(path.join(iconsDir, dir)).map(file => {
-            let name = path.basename(file, ".svg") + suffix
-            values[name] = { name, fullPath: path.join(iconsDir, dir, file) }
-          })
+    // Embeds Heroicons (https://heroicons.com) into your app.css bundle
+    // See your `CoreComponents.icon/1` for more information.
+    //
+    plugin(function ({ matchComponents, theme }) {
+      let iconsDir = path.join(__dirname, "./vendor/heroicons/optimized")
+      let values = {}
+      let icons = [
+        ["", "/24/outline"],
+        ["-solid", "/24/solid"],
+        ["-mini", "/20/solid"]
+      ]
+      icons.forEach(([suffix, dir]) => {
+        fs.readdirSync(path.join(iconsDir, dir)).map(file => {
+          let name = path.basename(file, ".svg") + suffix
+          values[name] = { name, fullPath: path.join(iconsDir, dir, file) }
         })
-        matchComponents({
-          "hero": ({ name, fullPath }) => {
-            let content = fs.readFileSync(fullPath).toString().replace(/\r?\n|\r/g, "")
-            return {
-              [`--hero-${name}`]: `url('data:image/svg+xml;utf8,${content}')`,
-              "-webkit-mask": `var(--hero-${name})`,
-              "mask": `var(--hero-${name})`,
-              "mask-repeat": "no-repeat",
-              "background-color": "currentColor",
-              "vertical-align": "middle",
-              "display": "inline-block",
-              "width": theme("spacing.5"),
-              "height": theme("spacing.5")
-            }
-          }
-        }, { values })
       })
-    ]
-  }
+      matchComponents({
+        "hero": ({ name, fullPath }) => {
+          let content = fs.readFileSync(fullPath).toString().replace(/\r?\n|\r/g, "")
+          return {
+            [`--hero-${name}`]: `url('data:image/svg+xml;utf8,${content}')`,
+            "-webkit-mask": `var(--hero-${name})`,
+            "mask": `var(--hero-${name})`,
+            "mask-repeat": "no-repeat",
+            "background-color": "currentColor",
+            "vertical-align": "middle",
+            "display": "inline-block",
+            "width": theme("spacing.5"),
+            "height": theme("spacing.5")
+          }
+        }
+      }, { values })
+    })
+  ]
 }
