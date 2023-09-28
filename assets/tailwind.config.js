cdf12a86415250cdf252969c814a1a05da023f74
Oliver Tigges <oliver.tigges@inspired.consulting>
2023-09-28 20:15:45 +0200

    Use tailwind container instead of fixed widths

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -34,6 +34,16 @@ module.exports = {
         },
       },
     },
+    container: {
+        padding: {
+          DEFAULT: '1rem',
+          sm: '2rem',
+          md: '2rem',
+          lg: '4rem',
+          xl: '5rem',
+          '2xl': '6rem',
+        },
+      },
   },
   plugins: [
     require("@tailwindcss/forms"),
