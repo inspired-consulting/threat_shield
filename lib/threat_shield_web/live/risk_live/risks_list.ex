5a149a42981a36dd864246dbd4a78692326a6b8a
Oliver Tigges <oliver.tigges@inspired.consulting>
2024-01-06 20:01:10 +0100

    Refactored risk UI

--- lib/threat_shield_web/live/risk_live/risks_list.ex
+++ lib/threat_shield_web/live/risk_live/risks_list.ex
@@ -1,6 +1,15 @@
 defmodule ThreatShieldWeb.RiskLive.RisksList do
   use ThreatShieldWeb, :live_component
 
+  alias ThreatShield.AI
+  alias ThreatShield.Scope
+  alias ThreatShield.AI.AiSuggestion
+  alias ThreatShield.Risks
+  alias ThreatShield.Risks.Risk
+  alias ThreatShield.Threats
+  alias ThreatShield.Threats.Threat
+  alias ThreatShield.Accounts.User
+
   @moduledoc """
   This component renders a list of risks for a given threat.
   """
@@ -8,65 +17,129 @@ defmodule ThreatShieldWeb.RiskLive.RisksList do
   @impl true
   def render(assigns) do
     ~H"""
-    <div class="mt-4 px-8 py-6 bg-white rounded-lg shadow">
-      <.stacked_list_header>
-        <:name><%= dgettext("risks", "Risks") %></:name>
+    <div class="risks">
+      <div class="mt-4 px-8 py-6 bg-white rounded-lg shadow">
+        <.stacked_list_header>
+          <:name><%= dgettext("risks", "Risks") %></:name>
 
-        <:subtitle>
-          <%= dgettext("risks", "Risk: short description") %>
-        </:subtitle>
+          <:subtitle>
+            <%= dgettext("risks", "Risk: short description") %>
+          </:subtitle>
 
-        <:buttons>
-          <.link
-            :if={ThreatShield.Members.Rights.may(:create_risk, @membership)}
-            patch={@path_prefix <> "/threats/#{@threat.id}/risks/new"}
-          >
-            <.button_primary>
-              <.icon name="hero-hand-raised" class="mr-1 mb-1" /><%= dgettext(
-                "risks",
-                "New Risk"
-              ) %>
-            </.button_primary>
-          </.link>
-          <.link>
-            <.button_magic
-              :if={ThreatShield.Members.Rights.may(:create_threat, @membership)}
-              disabled={not is_nil(@asking_ai_for_risks)}
-              phx-click="suggest_risks"
-              phx-value-threat_id={@threat.id}
+          <:buttons>
+            <.link
+              :if={ThreatShield.Members.Rights.may(:create_risk, @scope.membership)}
+              patch={@origin <> "/risks/new"}
             >
-              <.icon name="hero-sparkles" class="mr-1 mb-1" /><%= dgettext(
-                "risks",
-                "Suggest Risks"
-              ) %>
-            </.button_magic>
-          </.link>
-        </:buttons>
-      </.stacked_list_header>
-      <.stacked_list
-        :if={not Enum.empty?(@risks)}
-        id={"risks_for_threat_#{@threat.id}"}
-        rows={@risks}
-        row_click={
-          fn risk -> JS.navigate(@path_prefix <> "/threats/#{@threat.id}/risks/#{risk.id}") end
-        }
-      >
-        <:col :let={risk}>
-          <%= risk.name %>
-        </:col>
-        <:col :let={risk}>
-          <.risk_status_badge status={risk.status} light={true} />
-        </:col>
-        <:col :let={risk}><%= risk.description %></:col>
-        <:col :let={risk}>
-          <.criticality_badge value={risk.severity} title={dgettext("risks", "Severity")} />
-        </:col>
-      </.stacked_list>
+              <.button_primary>
+                <.icon name="hero-hand-raised" class="mr-1 mb-1" /><%= dgettext(
+                  "risks",
+                  "New Risk"
+                ) %>
+              </.button_primary>
+            </.link>
+            <.link>
+              <.button_magic
+                :if={ThreatShield.Members.Rights.may(:create_risk, @scope.membership)}
+                phx-click="suggest_risks"
+                phx-target={@myself}
+              >
+                <.icon name="hero-sparkles" class="mr-1 mb-1" /><%= dgettext(
+                  "risks",
+                  "Suggest Risks"
+                ) %>
+              </.button_magic>
+            </.link>
+          </:buttons>
+        </.stacked_list_header>
+        <.stacked_list
+          :if={not Enum.empty?(@risks)}
+          id={"risks_for_threat_#{@threat.id}"}
+          rows={@risks}
+          row_click={fn risk -> JS.navigate(@origin <> "/risks/#{risk.id}") end}
+        >
+          <:col :let={risk}>
+            <%= risk.name %>
+          </:col>
+          <:col :let={risk}>
+            <.risk_status_badge status={risk.status} light={true} />
+          </:col>
+          <:col :let={risk}><%= risk.description %></:col>
+          <:col :let={risk}>
+            <.criticality_badge value={risk.severity} title={dgettext("risks", "Severity")} />
+          </:col>
+        </.stacked_list>
 
-      <p :if={Enum.empty?(@risks)} class="mt-4">
-        There are no risks. Please add them manually or let the AI assistant make some suggestions.
-      </p>
+        <p :if={Enum.empty?(@risks)} class="mt-4">
+          There are no risks. Please add them manually or let the AI assistant make some suggestions.
+        </p>
+      </div>
+      <.modal
+        :if={assigns[:show_suggest_dialog] == true}
+        id="suggest-risks-modal"
+        show
+        on_cancel={JS.navigate(@origin)}
+      >
+        <.suggestions_dialog
+          title={dgettext("risks", "Suggested Risks")}
+          listener={@myself}
+          scope={@scope}
+          suggestions={@ai_suggestions[:risks]}
+        />
+      </.modal>
     </div>
     """
+  end
+
+  @impl true
+  def update(assigns, socket) do
+    socket
+    |> assign(assigns)
+    |> ok()
+  end
+
+  @doc """
+  Will start a background task to suggest risks for the current threat.
+  When the task is finished, it will send a :new_ai_suggestion message to the current page.
+  The page is expected to add the suggestions to the :ai_suggesstions assigns.
+  """
+  @impl true
+  def handle_event("suggest_risks", _params, socket) do
+    scope = %Scope{} = socket.assigns.scope
+    threat = socket.assigns.threat
+
+    Task.Supervisor.async_nolink(ThreatShield.TaskSupervisor, fn ->
+      new_risks =
+        AI.suggest_risks_for_threat(threat)
+
+      {:new_ai_suggestion, %AiSuggestion{result: new_risks, type: :risks, requestor: self()}}
+    end)
+
+    socket
+    |> assign(:show_suggest_dialog, true)
+    |> noreply()
+  end
+
+  @impl true
+  def handle_event("apply_selection", %{"selected_suggestions" => selected_names}, socket) do
+    scope = %Scope{} = socket.assigns.scope
+    threat = socket.assigns.threat
+
+    ai_suggestions = socket.assigns.ai_suggestions
+
+    new_risks =
+      ai_suggestions[:risks]
+      |> Enum.filter(fn s -> Enum.member?(selected_names, s.name) end)
+      |> Enum.map(fn s -> create_risk(scope.user, threat, s) end)
+
+    socket
+    |> assign(:show_suggest_dialog, false)
+    |> assign(:risks, socket.assigns.risks ++ new_risks)
+    |> noreply()
+  end
+
+  defp create_risk(%User{} = user, %Threat{} = threat, %{name: name, description: desc}) do
+    {:ok, risk} = Risks.add_risk(user, threat.id, name, desc)
+    risk
   end
 end
--- lib/threat_shield_web/live/threat_live/show.ex
+++ lib/threat_shield_web/live/threat_live/show.ex
@@ -1,10 +1,10 @@
 defmodule ThreatShieldWeb.ThreatLive.Show do
   use ThreatShieldWeb, :live_view
 
+  alias ThreatShield.Scope
   alias ThreatShield.Threats
+  alias ThreatShield.Threats.Threat
   alias ThreatShield.Risks.Risk
-  alias ThreatShield.Risks
-  alias ThreatShield.AI
   alias ThreatShield.Organisations.Organisation
 
   import ThreatShield.Organisations.Organisation, only: [list_system_options: 1]
@@ -14,7 +14,7 @@ defmodule ThreatShieldWeb.ThreatLive.Show do
   @impl true
   def mount(%{"threat_id" => threat_id} = params, _session, socket) do
     current_user = socket.assigns.current_user
-    threat = Threats.get_threat!(current_user, threat_id)
+    threat = %Threat{} = Threats.get_threat!(current_user, threat_id)
 
     socket =
       socket
@@ -23,15 +23,18 @@ defmodule ThreatShieldWeb.ThreatLive.Show do
       |> assign(:membership, Organisation.get_membership(threat.organisation, current_user))
       |> assign(:system, threat.system)
       |> assign(:page_title, page_title(socket.assigns.live_action))
-      |> assign(:asking_ai_for_risks, nil)
       |> assign(:risk_suggestions, [])
       |> assign(:called_via_system, Map.has_key?(params, "sys_id"))
+      |> assign(:ai_suggestions, %{})
 
     socket_with_options =
       if socket.assigns.called_via_system do
         socket
+        |> assign(:scope, Scope.for(current_user, threat.organisation, threat.system))
       else
-        assign(socket, :system_options, list_system_options(threat.organisation))
+        socket
+        |> assign(:scope, Scope.for(current_user, threat.organisation))
+        |> assign(:system_options, list_system_options(threat.organisation))
       end
 
     {:ok, socket_with_options}
@@ -71,40 +74,6 @@ defmodule ThreatShieldWeb.ThreatLive.Show do
   end
 
   @impl true
-  def handle_event("add_risk", %{"name" => name, "description" => description}, socket) do
-    user = socket.assigns.current_user
-    threat_id = socket.assigns.threat.id
-
-    {:ok, risk} = Risks.add_risk(user, threat_id, name, description)
-
-    suggestions =
-      Enum.filter(socket.assigns.risk_suggestions, fn s -> s.description != description end)
-      |> Enum.to_list()
-
-    stale_threat = socket.assigns.threat
-    updated_threat = %{stale_threat | risks: stale_threat.risks ++ [risk]}
-
-    {:noreply,
-     socket
-     |> assign(:threat, updated_threat)
-     |> assign(:risk_suggestions, suggestions)}
-  end
-
-  @impl true
-  def handle_event("ignore_risk", %{"description" => description}, socket) do
-    suggestions =
-      Enum.filter(socket.assigns.risk_suggestions, fn s -> s.description != description end)
-      |> Enum.to_list()
-
-    {:noreply, socket |> assign(:risk_suggestions, suggestions)}
-  end
-
-  @impl true
-  def handle_event("suggest_risks", %{"threat_id" => threat_id}, socket) do
-    {:noreply, start_suggestions(threat_id, socket)}
-  end
-
-  @impl true
   def handle_info({ThreatShieldWeb.RiskLive.RiskForm, {:saved, risk}}, socket) do
     stale_threat = socket.assigns.threat
     updated_threat = %{stale_threat | risks: stale_threat.risks ++ [risk]}
@@ -125,41 +94,23 @@ defmodule ThreatShieldWeb.ThreatLive.Show do
     {:noreply, socket}
   end
 
-  def handle_info({_from, {:ai_results, new_risks}}, socket) do
-    %{asking_ai_for_risks: ref} = socket.assigns
+  @impl true
+  def handle_info({task_ref, {:new_ai_suggestion, suggestion}}, socket) do
+    %{type: entity_type, result: result} = suggestion
 
-    Process.demonitor(ref, [:flush])
+    # stop monitoring the task
+    Process.demonitor(task_ref, [:flush])
 
-    {:noreply,
-     socket
-     |> assign(
-       asking_ai_for_risks: nil,
-       risk_suggestions: socket.assigns.risk_suggestions ++ new_risks
-     )}
+    suggestions =
+      (socket.assigns[:suggestions] || %{})
+      |> Map.put(entity_type, result)
+
+    socket
+    |> assign(ai_suggestions: suggestions)
+    |> noreply()
   end
 
   defp page_title(:show), do: "Show Threat"
   defp page_title(:edit_threat), do: "Edit Threat"
   defp page_title(:new_risk), do: "New Risk"
-
-  defp start_suggestions(threat_id, socket) do
-    user = socket.assigns.current_user
-
-    task =
-      Task.Supervisor.async_nolink(ThreatShield.TaskSupervisor, fn ->
-        ask_ai(user, threat_id)
-      end)
-
-    socket
-    |> assign(asking_ai_for_risks: task.ref)
-  end
-
-  defp ask_ai(user, threat_id) do
-    threat = Threats.get_threat!(user, threat_id)
-
-    risk_descriptions =
-      AI.suggest_risks_for_threat(threat)
-
-    {:ai_results, risk_descriptions}
-  end
 end
